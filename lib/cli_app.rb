a84027399ccff02905c11720f5c9f83446774309
hvts83 <herberthtejada@gmail.com>
2019-10-02 16:39:13 -0400

    update

--- lib/cli_app.rb
+++ lib/cli_app.rb
@@ -12,6 +12,8 @@ class CliApp
 
     end
 
+   
+
     def welcome
         puts "Welcome to Clerkbuster."
     end
@@ -19,8 +21,34 @@ class CliApp
     def main_menu_select
         prompt = TTY::Prompt.new
 
-        choices = ["Rent a movie", "A", "B"]
+        choices = ["Rent a movie", "New Member", "Check your Membership", "Return Movie","Exit"]
         prompt.select("Select option?", choices)
+    end
+
+    def rent_movie
+        prompt = TTY::Prompt.new
+        choices = ["Search by Titles", "View all Movies"]
+        # prompt.select("Select option?", choices)
+
+        movie_selection = prompt.select("Select option?", choices)
+        case movie_selection
+        when "Search by Titles"
+            search_movies
+        else
+            view_all_movies
+        end
+        # binding.pry   
+    end
+
+    def search_movies
+        prompt = TTY::Prompt.new
+        prompt.ask("Search movie by title:", required: true)
+    end
+
+    def view_all_movies
+        prompt = TTY::Prompt.new
+        puts "ClerksBuster has all these amazing movies"
+        puts "========================================="
     end
 
 end  # end of CliApp
